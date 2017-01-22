e5d0bf197aa886c6e01cee51e31290a3156f10aa
Brent Adonis <Brent.Adonis@reckon.com>
2017-01-23 12:01:24 +1300

    Implemented HTTP verbs. Implemented middleware when searching for individual client ids.

--- src/models/clientModel.js
+++ src/models/clientModel.js
@@ -1,17 +1,19 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+var personalDetails = new Schema({
+    firstname: String,
+    middlename: String,
+    lastname: String,
+    salutation: String
+});
+
 var clientModel = new Schema({
     name: String,
     reportingname: String,
     tradingas: String,
     abbreviatedname: String,
-    persondetails: [{
-        firstname: String,
-        middlename: String,
-        lastname: String,
-        salutation: String
-    }],
+    persondetails: [personalDetails],
     entitytype: {
         type: String,
         enum: ['Company', 'Trust', 'Partnership', 'Individual', 'Society', 'Incorporated Society', 'Charitable Trust']
--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -9,7 +9,7 @@ var router = function(Client) {
             console.log(client);
             client.save();
             res.status(201).send(client);
-    })
+        })
 
         .get(function(req, res) {
             var query = [];
@@ -27,13 +27,77 @@ var router = function(Client) {
             });
         });
 
+    clientRouter.use('/:clientId', function(req, res, next) {
+        Client.findById(req.params.clientId, function(err, client) {
+                if (err) {
+                    res.status(500).send(err);
+                } else if (client) {
+                    req.client = client;
+                    next();
+                } else {
+                    res.status(404).send('No client has been found with that ID.');
+                }
+            });
+    });
+
     clientRouter.route('/:clientId')
         .get(function(req, res) {
-            Client.findById(req.params.clientId, function(err, client) {
+            res.json(req.client);
+        })
+
+        .put(function(req, res) {
+            req.client.name = req.body.name;
+            req.client.reportingname = req.body.reportingname;
+            req.client.tradingas = req.body.tradingas;
+            req.client.abbreviatedname = req.body.abbreviatedname;
+            req.client.entitytype = req.body.entitytype;
+            req.client.acn = req.body.acn;
+            req.client.country = req.body.country;
+            req.client.tax = req.body.tax;
+            req.client.taxnumber = req.body.taxnumber;
+            req.client.businessnumber = req.body.businessnumber;
+            req.client.ledgertext = req.body.ledgertext;
+            req.client.ledgervalue = req.body.ledgervalue;
+            req.client.datemodified = req.body.datemodified;
+            req.client.persondetails = req.body.persondetails;
+
+            console.log(req.client);
+
+            req.client.save(function(err) {
                 if (err) {
                     res.status(500).send(err);
                 } else {
-                    res.json(client);
+                    res.json(req.client);
+                }
+            });
+        })
+
+        .patch(function(req, res) {
+             if (req.body._id)
+                delete req.body._id;
+            
+            for (var p in req.body)
+            {
+                req.client[p] = req.body[p];
+            }
+            
+            console.log(req.client);
+
+            req.client.save(function(err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.client);
+                }
+            });
+        })
+
+        .delete(function(req, res) {
+            req.client.remove(function(err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.client);
                 }
             });
         });
